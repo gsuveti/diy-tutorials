6aedeacbd0a63446237343c09080782b8fe951e4
George Suveti <george.suveti@irian.ro>
2020-02-26 17:51:09 +0200

    Email retete #21 - fix template

--- functions/src/template.ts
+++ functions/src/template.ts
@@ -1,18 +1,26 @@
 // language=HTML
 export const template: string = `
     <style>
+        .container {
+            padding: 32px;
+            background-color: rgb(247, 247, 247);
+        }
 
         .content {
             max-width: 632px;
             margin: auto;
             padding: 16px;
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+            background-color: white;
             font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
             box-sizing: border-box;
             border-radius: 4px;
         }
 
-        .content .logo {
+        .header {
+            padding-bottom: 32px;
+        }
+
+        .header .logo {
             display: block;
             width: 200px;
             margin: auto;
@@ -23,26 +31,25 @@ export const template: string = `
         }
 
         .products, .optional-products {
-            display: flex;
-            flex-wrap: wrap;
+            display: table;
+            width: 100%;
         }
 
         .products .product, .optional-products .product {
-            display: flex;
-            flex-direction: column;
-            flex-basis: 200px;
+            display: inline-block;
+            width: 33.33%;
             padding: 2px;
             box-sizing: border-box;
-
         }
 
+
         .product-content {
-            display: flex;
-            flex-direction: column;
+            display: block;
             border: 1px solid gray;
             border-radius: 2px;
+            height: 200px;
             padding: 0 8px;
-            flex-grow: 1;
+            overflow: hidden;
         }
 
         .product h3 {
@@ -54,72 +61,73 @@ export const template: string = `
             padding-top: 8px;
         }
 
-        .figure {
-            max-height: 200px;
-            display: flex;
-        }
 
         .instances-question label, .measurement-label {
-            display: flex;
-            justify-content: space-between;
+            display: block;
             border: 1px solid #cacaca;
             padding: 4px;
             margin: 2px 0;
             border-radius: 2px;
+
         }
 
         .instances-question label > *, .measurement-label > * {
-            display: flex;
-            flex-basis: 50%;
+            display: inline-block;
+            width: 50%;
             border-left: 1px solid #cacaca;
             padding-left: 4px;
+            float: right;
         }
 
 
     </style>
-
-    <div class="content">
-        <img class="logo" src="https://lucrareamea.ro/wp-content/uploads/2019/10/Logo-Lucrareamea-redim.png">
-
-        <h1><%= title%></h1>
-        <%- content %>
-        <%- displayedSections %>
-
-        <br>
-
-        <%if (requiredProductsTotal > 0) { %>
-        <h2 class="products-title">Produse necesare (<%= requiredProductsTotal %> lei)</h2>
-        <div class="products">
-            <% requiredProducts.forEach(function(product){ %>
-            <div class="product">
-                <div class="product-content">
-                    <h3><a target="_blank" href="<%= product.url %>"><%= product.headline %></a> x <%=
-                        productQuantities[product.uuid] %></h3>
-                    <figure>
-                        <img src="<%= product.imageUrl %>"/>
-                    </figure>
-                </div>
-            </div>
-            <% }); %>
+    <div class="container">
+        <div class="header">
+            <img class="logo" src="https://lucrareamea.ro/wp-content/uploads/2019/10/Logo-Lucrareamea-redim.png">
         </div>
-        <% } %>
+        <div class="content">
 
-        <%if (optionalProductsTotal > 0) { %>
-        <h2 class="optional-products-title">Produse opționale (<%= optionalProductsTotal %> lei)</h2>
-        <div class="optional-products">
-            <% optionalProducts.forEach(function(product){ %>
-            <div class="product">
-                <div class="product-content">
-                    <h3><a target="_blank" href="<%= product.url %>"><%= product.headline %></a> x <%=
-                        productQuantities[product.uuid] %></h3>
-                    <figure>
-                        <img src="<%= product.imageUrl %>"/>
-                    </figure>
+            <h1><%= title%></h1>
+            <%- content %>
+            <%- displayedSections %>
+
+            <br>
+
+            <%if (requiredProductsTotal > 0) { %>
+            <div class="products">
+                <h2 class="products-title">Produse necesare (<%= requiredProductsTotal %> lei)</h2>
+
+                <% requiredProducts.forEach(function(product){ %>
+                <div class="product">
+                    <div class="product-content">
+                        <h3><a target="_blank" href="<%= product.url %>"><%= product.headline %></a> x <%=
+                            productQuantities[product.uuid] %></h3>
+                        <figure>
+                            <img src="<%= product.imageUrl %>"/>
+                        </figure>
+                    </div>
                 </div>
+                <% }); %>
             </div>
-            <% }); %>
+            <% } %>
+
+            <%if (optionalProductsTotal > 0) { %>
+            <div class="optional-products">
+                <h2 class="optional-products-title">Produse opționale (<%= optionalProductsTotal %> lei)</h2>
+
+                <% optionalProducts.forEach(function(product){ %>
+                <div class="product">
+                    <div class="product-content">
+                        <h3><a target="_blank" href="<%= product.url %>"><%= product.headline %></a> x <%=
+                            productQuantities[product.uuid] %></h3>
+                        <figure>
+                            <img src="<%= product.imageUrl %>"/>
+                        </figure>
+                    </div>
+                </div>
+                <% }); %>
+            </div>
+            <% } %>
         </div>
-        <% } %>
     </div>
-
 `;
