4715f7e940aa8ae34b71c6f813a28377eea64343
George Suveti <george.suveti@irian.ro>
2020-05-20 13:04:45 +0300

    Update product quantities even if there are no measurements #30

--- libs/diy-tutorials-common/src/lib/+state/epics/calculate-product-quantities.epic.ts
+++ libs/diy-tutorials-common/src/lib/+state/epics/calculate-product-quantities.epic.ts
@@ -2,42 +2,47 @@ import {Observable} from 'rxjs';
 import {AnyAction} from 'redux';
 import {ofType, StateObservable} from 'redux-observable';
 import {AddMeasurement, TutorialActionTypes, updateProductQuantities} from '../tutorial.actions';
-import {map} from 'rxjs/operators';
+import {debounceTime, map} from 'rxjs/operators';
 import {AppState} from '@diy-tutorials/diy-tutorials-common';
 
 const FormulaParser = require('hot-formula-parser').Parser;
 
 export const calculateProductQuantitiesEpic = (action$: Observable<AnyAction>, state$: StateObservable<AppState>) => {
-  return action$.pipe(
-    ofType(TutorialActionTypes.UpdateMeasurementFormValue),
-    map((action: AddMeasurement) => {
-      const state = state$.value;
-      const productQuantities = calculateProductQuantities(state);
-      return updateProductQuantities(productQuantities);
-    })
-  );
+    return action$.pipe(
+        ofType(
+            TutorialActionTypes.UpdateMeasurementFormValue,
+            TutorialActionTypes.UserDataFetched,
+            TutorialActionTypes.ShowProductsOrScrollToMeasurements
+        ),
+        debounceTime(300),
+        map((action: AddMeasurement) => {
+            const state = state$.value;
+            const productQuantities = calculateProductQuantities(state);
+            return updateProductQuantities(productQuantities);
+        })
+    );
 };
 
 
 function calculateProductQuantities(state: AppState) {
-  const tutorialState = state.tutorial;
-  const userContextState = state.userContext;
-  const measuredFormValues = tutorialState.measurementFormsOrder.map(uuid => userContextState.measuredFormValues[uuid]);
+    const tutorialState = state.tutorial;
+    const userContextState = state.userContext;
+    const measuredFormValues = tutorialState.measurementFormsOrder.map(uuid => userContextState.measuredFormValues[uuid]);
 
-  return tutorialState.products.reduce((productQuantities, block) => {
-    const parser = new FormulaParser();
-    parser.on('callCellValue', function (cellCoord, done) {
-      done(measuredFormValues[cellCoord.row.index]);
-    });
+    return tutorialState.products.reduce((productQuantities, block) => {
+        const parser = new FormulaParser();
+        parser.on('callCellValue', function (cellCoord, done) {
+            done(measuredFormValues[cellCoord.row.index]);
+        });
 
-    const parsedObject = parser.parse(block.quantityFormula);
-    const quantity = parsedObject.error ? 0 : parsedObject.result;
+        const parsedObject = parser.parse(block.quantityFormula);
+        const quantity = parsedObject.error ? 0 : parsedObject.result;
 
-    return {
-      ...productQuantities,
-      [block.uuid]: Math.ceil(quantity)
-    }
+        return {
+            ...productQuantities,
+            [block.uuid]: Math.ceil(quantity)
+        }
 
-  }, {});
+    }, {});
 
 }
